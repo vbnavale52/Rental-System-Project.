7d216fdbba87dd476ca1331d2df6346c5bbd7642
samadhan563 <samadhan563@gmail.com>
2021-04-22 14:05:55 +0530

    First Commit

---
+++ Rental-System-Project/src/main/java/com/rsys/RentalSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalSystemProjectApplication.class, args);
+	}
+
+}
